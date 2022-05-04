9bfcbb211d884fca301d28bade378fdc1dc8a798
Leozerr <patchanon-leo@hotmail.com>
2022-05-04 23:48:35 +0700

    sprite test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # import pygame
 import pygame
 
+from FishData import FishData
+
 class Crosshair(pygame.sprite.Sprite):
     def __init__(self, pic_path):
         super().__init__()
@@ -9,10 +11,48 @@ class Crosshair(pygame.sprite.Sprite):
     def update(self):
         self.rect.center = pygame.mouse.get_pos()
 
-class Fish(pygame.sprite.Sprite):
-    def __init__(self, pic_path, pos_x, pos_y):
-        super().__init__()
-        self.image
+class Fish(pygame.sprite.Sprite, FishData):
+    swimRight = [pygame.image.load('images/R1E.png'),pygame.image.load('images/R2E.png'),pygame.image.load('images/R3E.png'),pygame.image.load('images/R4E.png'),pygame.image.load('images/R5E.png'),pygame.image.load('images/R6E.png'),pygame.image.load('images/R7E.png'),pygame.image.load('images/R11E.png')]#,pygame.image.load('images/R8E.png'),pygame.image.load('images/R9E.png'),pygame.image.load('images/R10E.png'),pygame.image.load('images/R11E.png')]
+    swimLeft = [pygame.image.load('images/L1E.png'),pygame.image.load('images/L2E.png'),pygame.image.load('images/L3E.png'),pygame.image.load('images/L4E.png'),pygame.image.load('images/L5E.png'),pygame.image.load('images/L6E.png'),pygame.image.load('images/L7E.png'),pygame.image.load('images/L11E.png')]#,pygame.image.load('images/L8E.png'),pygame.image.load('images/L9E.png'),pygame.image.load('images/L10E.png'),pygame.image.load('images/L11E.png')]
+    
+    def __init__(self, pos_x, pos_y, width, height, end):
+        self.x = pos_x
+        self.y = pos_y
+        self.width = width
+        self.height = height
+        self.end = end
+        self.path = [100, 955] #distance of fish
+        self.swimCount = 0
+        self.vel = 3
+    
+    def draw(self, screen):
+        self.move()
+        if self.swimCount + 1 >= 22:
+            self.swimCount = 0
+
+        if self.vel > 0:
+            screen.blit(self.swimRight[self.swimCount //3], (self.x, self.y))
+            self.swimCount += 1
+        else:
+            screen.blit(self.swimLeft[self.swimCount //3], (self.x, self.y))
+            self.swimCount += 1
+
+    def move(self):
+        if self.vel > 0:
+            if self.x + self.vel < self.path[1]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.swimCount = 0
+        else:
+            if self.x - self.vel > self.path[0]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.swimCount = 0
+        #print("x:" + str(self.x) + " count:" + str(self.swimCount))
+
+        
 # initialize game engine
 class main() :
     pygame.init()
@@ -20,7 +60,7 @@ class main() :
     window_width=999
     window_height=674
 
-    animation_increment=10
+    #animation_increment=10
     clock_tick_rate=20
 
     # Open a window
@@ -41,6 +81,9 @@ class main() :
     crosshair_group = pygame.sprite.Group()
     crosshair_group.add(crosshair)
 
+    # Fish
+    fish = Fish(100, 410, 64, 64, 450)
+
     while(dead==False):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -49,6 +92,7 @@ class main() :
         screen.blit(background_image, [0, 0])
         crosshair_group.draw(screen)
         crosshair_group.update()
+        fish.draw(screen)
 
         pygame.display.flip()
         clock.tick(clock_tick_rate)
